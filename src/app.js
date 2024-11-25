9b0a13c7dcfc16b7c2218f466788b7f18c301bc0
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-11-25 14:08:57 +0300

    update app.js to use routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const booksRoutes = require('./routes/booksRoutes'); 
+const bodyParser = require('body-parser');
+
+dotenv.config();
 
 const app = express();
 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+app.use('/api', booksRoutes); 
+
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to the Books Collection');
+  res.status(200).send('Welcome to the Books Collection API!');
+});
+
+
+app.use((req, res, next) => {
+  res.status(404).json({ success: false, message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ success: false, message: 'Something went wrong!' });
 });
 
 const PORT = process.env.PORT || 3000;

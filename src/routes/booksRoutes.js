a9b774ba1a64ba31c934118f6cfaae90e5264403
Sura Itana <suraitana@gmail.com>
2024-12-09 16:42:45 +0300

    use authenticateToken and authorizeRole middleware to protect routes

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { getAllBooks, addBook, getBookById, updateBook, deleteBook, getRecommendations } = require('../controllers/booksController');
+const { authorizeRole, authenticateToken } = require('../middlewares/middlewares');
 const router = express.Router();
 
-router.get('/books', getAllBooks);
-router.post('/books', addBook);
-router.get('/books/recommendations', getRecommendations);
-router.get('/books/:id', getBookById);
-router.put('/books/:id', updateBook);
-router.delete('/books/:id', deleteBook);
+router.get('/books/all', authenticateToken, authorizeRole('admin') ,getAllBooks);
+router.post('/books', authenticateToken, authorizeRole('user'), addBook);
+router.get('/books/recommendations', authenticateToken, getRecommendations);
+router.get('/books/:id', authenticateToken, getBookById);
+router.put('/books/:id',authenticateToken, updateBook);
+router.delete('/books/:id', authenticateToken, deleteBook);
 
 
 module.exports = router;

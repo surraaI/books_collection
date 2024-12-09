a729c85ab5a7cee1d188ba54fbc84ba33fd99274
Sura Itana <suraitana@gmail.com>
2024-12-09 16:41:48 +0300

    add create_admin and assign_admin routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { login, logout, signup } = require('../controllers/authController');
+const { login, logout, signup, create_admin ,assign_admin } = require('../controllers/authController');
+const { authenticateToken } = require('../middlewares/middlewares');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.get('/logout', logout);
+router.post('/create_admin', authenticateToken, create_admin);
+router.post('/assign_admin', authenticateToken, assign_admin);
 
 module.exports = router;
